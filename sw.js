a1757412d7b62ee6f8cdb69b195860fee5a4dd8d
n5nkprg4j8-cmd <n5nkprg4j8@privaterelay.appleid.com>
2026-07-20 07:45:46 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-// placeholder
+const CACHE="ml-analyzer-v2";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch",event=>{
+  event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request)));
+});
